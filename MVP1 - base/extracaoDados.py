5df5e21ebf1f9b51abcb5061f517888ea72c329d
Beatriz Manaia Lourenço Berto <130764598+beatrizmanaia26@users.noreply.github.com>
2026-04-24 02:25:53 +0000

    inicio mvp

---
+++ MVP1 - base/extracaoDados.py
@@ -0,0 +1,55 @@
+#deteccao de questoes no enem: ler pdf e achar onde ta escrito "QUESTAO 0....""
+
+#pip install pdfplumber#extrai texto de forma estruturada 
+import pdfplumber #extrai texto e imagens do pdf
+import os #interage com so (permite ver arquivos,pastas,navegar entre diretorios...)
+import re #permite usar padroes de texto (regex)
+
+pasta = "../enems"
+
+#modo especifico (um arquivo)
+arquivo = "enem2025Dia1Regular.pdf"
+caminhoCompleto = os.path.join(pasta, arquivo)
+
+print(f"\n Lendo: {arquivo}")
+
+paginas = [] #guarda texto + info de cada pagina
+
+with pdfplumber.open(caminhoCompleto) as pdf: #com with abre arquivo, usa como pdf, executa o bloco e fecha automaticamente
+    
+    for i, pagina in enumerate(pdf.pages): #percorre cada pagina do pdf
+        texto = pagina.extract_text() #extrai texto da pagina
+
+        paginas.append({
+            "numero_pagina": i,
+            "texto": texto if texto else "",
+            "tem_imagem": bool(pagina.images)
+        })
+
+#junta todo texto
+texto_total = "\n".join(p["texto"] for p in paginas)
+
+#separa questoes
+questoes = re.split(r'QUESTÃO\s+\d+', texto_total, flags=re.IGNORECASE)
+questoes = [q.strip() for q in questoes if q.strip()]
+
+# quantidade total de questões
+print(f"\nTotal de questões: {len(questoes)}")
+
+# info por questão
+for i, questao in enumerate(questoes):
+    numero_questao = i + 1
+
+    qtd_palavras = len(questao.split())
+
+    # tenta descobrir se a questão tem imagem
+    tem_imagem = False
+    for p in paginas:
+        if questao[:100] in p["texto"]:
+            if p["tem_imagem"]:
+                tem_imagem = True
+                break
+
+    print(f"\nQuestão {numero_questao}")
+    print(f"Quantidade de palavras: {qtd_palavras}")
+    print(f"Tem imagem: {tem_imagem}")
